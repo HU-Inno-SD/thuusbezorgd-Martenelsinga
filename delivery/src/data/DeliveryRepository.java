37e5c7ed92e8598433db8cf6dd42162c3153db34
Martenelsinga <74406658+Martenelsinga@users.noreply.github.com>
2023-01-24 12:07:46 +0100

    1

---
+++ delivery/src/data/DeliveryRepository.java
@@ -0,0 +1,16 @@
+package data;
+
+import domain.Delivery;
+import nl.hu.inno.thuusbezorgd.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+    @Query(name = "Delivery.findRandom")
+    Optional<Delivery> findRandomDelivery();
+
+    List<Delivery> findByOrder_User(User user);
+}
